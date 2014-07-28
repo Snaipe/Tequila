e086c8222db1814199e11b8b1ba0746cebeca332
Snaipe <franklinmathieu@gmail.com>
2014-07-28 06:46:55 +0200

    Added fixed-size download statement/bar

--- tequila/download.py
+++ tequila/download.py
@@ -40,23 +40,40 @@ def checksum(file):
             raise ChecksumNotMatchingError(sum1, sum2)
 
 
+def bytes_to_human(bytes):
+    suffixes = ['k', 'M', 'G', 'T', 'P']
+    n, s = bytes, ''
+    for suffix in suffixes:
+        bytes /= 1024.0
+        if bytes < 1:
+            break
+        n, s = bytes, suffix
+    return ('%.2f' % n)[:4].strip('0.') + ' %sB' % s
+
+
 def download(urlopener, name, url, target, validate=False):
-    bar_size = 20
+    max_name_size = 26
+    bar_size = 15
     last = 0
 
     def reporthook(blocknum, blocksize, totalsize):
         nonlocal last
+        nonlocal name
 
         # skip first handling to let propagate any error
         if blocknum == 0:
             return
+
+        if len(name) > max_name_size:
+            half = math.floor(max_name_size / 2)
+            name = '...'.join([name[:half], name[-half:]])
 
         percent = min(1e2, blocknum * blocksize * 1e2 / totalsize)
         progress = min(bar_size, math.floor(blocknum * blocksize * bar_size / totalsize))
 
         if blocknum == 1:
             # dirty hack until I find the time to make this better
-            sys.stdout.write('Downloading %s (%d Bytes): [%s]   0%%' % (name, totalsize, ' ' * bar_size))
+            sys.stdout.write('Downloading %s (%s): [%s]   0%%' % (name, bytes_to_human(totalsize), ' ' * bar_size))
             sys.stdout.write('\033[%dD' % (bar_size + 6))
 
         if last < progress:
